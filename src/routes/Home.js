37f86837a98a13647c0ce839ea33b254a320b26f
Tubt <tu.bui@gooddata.com>
2019-10-15 13:37:06 +0700

    check export

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,25 +6,38 @@ import constants from "../constants";
 import { project } from "../project";
 
 const Home = () => {
-    const [ exportState, setExportState ] = useState({ error: null, isLoading: true, data: null });
+    const [exportState, setExportState] = useState({ error: null, isLoading: true, data: null });
 
-    const onExportReady = (currentGetExportResult) => {
-        currentGetExportResult({}).then((result) => {
-            setExportState({ error: null, isLoading: false, data: result.uri })
-        }).catch((error) => {
-            console.error(error);
-            setExportState({ error, isLoading: false, data: null })
-        })
-    }
+    const onExportReady = currentGetExportResult => {
+        currentGetExportResult({})
+            .then(result => {
+                setExportState({ error: null, isLoading: false, data: result.uri });
+            })
+            .catch(error => {
+                console.error(error);
+                setExportState({ error, isLoading: false, data: null });
+            });
+    };
 
-    const visUri = `/gdc/md/${constants.projectId}/obj/75548`;
-
-    return <Page>
-        <div style={{ height: 400 }} >
-            <Visualization {...project} onExportReady={onExportReady} uri={visUri}/>
-        </div>
-        {exportState.data !== null ? <a download={exportState.data} href={exportState.data} >Download Export</a> : <p>Preparing export...</p>}
-    </Page>;
+    const visUriChart = `/gdc/md/${constants.projectId}/obj/75548`;
+    const visUriTable = `/gdc/md/${constants.projectId}/obj/75549`;
+    return (
+        <Page>
+            <div style={{ height: 400 }}>
+                <Visualization {...project} onExportReady={onExportReady} uri={visUriChart} />
+            </div>
+            <div style={{ height: 400 }}>
+                <Visualization {...project} onExportReady={onExportReady} uri={visUriTable} />
+            </div>
+            {exportState.data !== null ? (
+                <a download={exportState.data} href={exportState.data}>
+                    Download Export
+                </a>
+            ) : (
+                <p>Preparing export...</p>
+            )}
+        </Page>
+    );
 };
 
 export default Home;

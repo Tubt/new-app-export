3d608ea2aa8e85b3f07501af316a3599f2405fed
Tubt <tu.bui@gooddata.com>
2019-10-21 13:40:16 +0700

    add pivot table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "@gooddata/react-components/styles/css/main.css";
-import { Visualization, PivotTable, Model } from "@gooddata/react-components";
+import { Visualization } from "@gooddata/react-components";
 import { factory as sdkFactory } from "@gooddata/gooddata-js";
 import "./App.css";
 import ExampleWithExport from "./ExampleWithExport";
@@ -12,7 +12,7 @@ class App extends Component {
         const whiteLabeledDomain = "zebroids.intgdc.com";
         this.sdk = sdkFactory({ domain: whiteLabeledDomain }); // this needs to be provided as a prop to the Visualization component in render method
         this.projectId = "ltn06hvt07uko2r87itmnoaibgzc0mkn"; // this needs to be project on whitelabeled domain
-        this.visId = "75548"; // this needs to be some chart visualization NOT table!!!
+        this.visId = "75549"; // this needs to be some chart visualization NOT table!!!
         // TODO once domain where this app is deployed is enabled for CORS on whitelabeled domain remove any PROXY settings
     }
 
@@ -24,33 +24,35 @@ class App extends Component {
                         <ExampleWithExport>
                             {onExportReady => (
                                 <div style={{ width: 600, height: 800 }}>
-                                    <Visualization
+                                    {/* <Visualization
                                         projectId={this.projectId}
                                         uri={`/gdc/md/${this.projectId}/obj/${this.visId}`}
+                                        sdk={this.sdk}
+                                        onExportReady={onExportReady}
+                                    /> */}
+                                    <PivotTable
+                                        measures={[
+                                            Model.measure("aagJGHg1bxap")
+                                                .localIdentifier("m_1")
+                                                .format(
+                                                    "[>=30000000][color=2190c0]█████ #,##0; [>=25000000][color=2190c0]████░ #,##0; [>=10000000][color=2190c0]███░░ #,##0; [>=9000000][color=2190c0]██░░░ #,##0; [>=0][color=2190c0]█░░░░ #,##0; [=Null] No data;",
+                                                ),
+                                        ]}
+                                        rows={[
+                                            Model.attribute(
+                                                "fiscaldate.fiscaljun1_aag81lMifn6q",
+                                            ).localIdentifier("va_4"),
+                                            Model.attribute(
+                                                "label.restaurantlocation.locationresort",
+                                            ).localIdentifier("va_5"),
+                                        ]}
+                                        projectId={this.projectId}
                                         sdk={this.sdk}
                                         onExportReady={onExportReady}
                                     />
                                 </div>
                             )}
                         </ExampleWithExport>
-                        <div style={{ height: 500 }}>
-                            <PivotTable
-                                measures={[
-                                    Model.measure("alUEwmBtbwSh")
-                                        .localIdentifier("m_0")
-                                        .format(
-                                            "30000000][color=2190c0]█████ #,##0; [>=25000000][color=2190c0]████░ #,##0; [>=10000000][color=2190c0]███░░ #,##0; [>=9000000][color=2190c0]██░░░ #,##0; [>=0][color=2190c0]█░░░░ #,##0; [=Null] No data;",
-                                        ),
-                                ]}
-                                rows={[
-                                    Model.attribute("label.owner.department").localIdentifier("va_2"),
-                                    Model.attribute("label.owner.id.name").localIdentifier("va_4"),
-                                    Model.attribute("label.product.id.name").localIdentifier("va_5"),
-                                ]}
-                                columns={[Model.attribute("label.owner.department").localIdentifier("va_3")]}
-                                projectId="ltn06hvt07uko2r87itmnoaibgzc0mkn"
-                            />
-                        </div>
                     </header>
                 </div>
             </div>

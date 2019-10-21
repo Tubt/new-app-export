9238bb076b5ab94f891910e55f18a5f3d6a0a4a8
Tubt <tu.bui@gooddata.com>
2019-10-21 14:45:53 +0700

    add visualization chart

--- src/App.js
+++ src/App.js
@@ -24,29 +24,9 @@ class App extends Component {
                         <ExampleWithExport>
                             {onExportReady => (
                                 <div style={{ width: 600, height: 800 }}>
-                                    {/* <Visualization
+                                    <Visualization
                                         projectId={this.projectId}
                                         uri={`/gdc/md/${this.projectId}/obj/${this.visId}`}
-                                        sdk={this.sdk}
-                                        onExportReady={onExportReady}
-                                    /> */}
-                                    <PivotTable
-                                        measures={[
-                                            Model.measure("aagJGHg1bxap")
-                                                .localIdentifier("m_1")
-                                                .format(
-                                                    "[>=30000000][color=2190c0]█████ #,##0; [>=25000000][color=2190c0]████░ #,##0; [>=10000000][color=2190c0]███░░ #,##0; [>=9000000][color=2190c0]██░░░ #,##0; [>=0][color=2190c0]█░░░░ #,##0; [=Null] No data;",
-                                                ),
-                                        ]}
-                                        rows={[
-                                            Model.attribute(
-                                                "fiscaldate.fiscaljun1_aag81lMifn6q",
-                                            ).localIdentifier("va_4"),
-                                            Model.attribute(
-                                                "label.restaurantlocation.locationresort",
-                                            ).localIdentifier("va_5"),
-                                        ]}
-                                        projectId={this.projectId}
                                         sdk={this.sdk}
                                         onExportReady={onExportReady}
                                     />
